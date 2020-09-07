ce9c06f81cb7acaac4e28a0557c25ec2d374660f
Gavin Rozzi <gr@gavinrozzi.com>
2020-09-07 14:58:50 -0400

    Add tract function, update documentation

--- R/zip_lookups.r
+++ R/zip_lookups.r
@@ -3,10 +3,10 @@
 #'
 #' @param state_abb Two-digit code for a U.S. state
 #' @return dataframe of all ZIP codes for state defined in state_abb
-#'
 #' @examples
-#' 'NJ <- zipcodeR::search_state('NJ')'
-#' 'CA <- zipcodeR::search_state('CA')'
+#' search_state('NJ')
+#' search_state('CA')
+#'
 #' @export
 search_state <- function(state_abb){
   # Test if state abbreviation input is capitalized, capitalize if lowercase
@@ -31,8 +31,8 @@ search_state <- function(state_abb){
 #' @return dataframe of all ZIP codes found for given county
 #'
 #' @examples
-#' 'middlesex <- search_county('Middlesex','NJ')'
-#' 'alameda <- search_county('alameda','CA')'
+#' middlesex <- search_county('Middlesex','NJ')
+#' alameda <- search_county('alameda','CA')
 #' @export
 search_county <- function(county_name, state_abb) {
   # Test if state abbreviation input is capitalized, capitalize if lowercase
@@ -63,7 +63,8 @@ search_county <- function(county_name, state_abb) {
 #' @return dataframe of all ZIP codes found for given timezone
 #'
 #' @examples
-#' 'eastern <- search_county('Eastern')'
+#' eastern <- search_tz('Eastern')
+#' pacific <- search_tz('Mountain')
 #' @export
 search_tz <- function(tz) {
   # Get matching ZIP codes for timezone
@@ -76,3 +77,27 @@ search_tz <- function(tz) {
   print(paste(nrow(tz_zips), 'ZIP codes found for', tz,'timezone'))
   return(tz_zips)
 }
+#' Get all Census tracts within a given ZIP code
+#'
+#' @param zip_code A U.S. ZIP code
+#' @return dataframe of Census tracts and data from Census crosswalk file found for given ZIP code
+#'
+#' @examples
+#' get_tracts('08731')
+#' get_tracts('90210')
+#' @export
+get_tracts <- function(zip_code) {
+  # Validate input, raise error if input is not a 5-digit ZIP code
+  if (nchar(zip_code) != 5) {
+    stop("Invalid input detected. Please enter a 5-digit U.S. ZIP code.")
+  }
+  # Get tract data given ZCTA
+  tracts <- zcta_crosswalk %>% dplyr::filter(ZCTA5 == zip_code)
+  if (nrow(tracts) == 0) {
+    stop(paste("No Census tracts found for ZIP code", zip_code))
+  }
+  # Print number of tracts found to console
+  print(paste(nrow(tracts), 'Census tracts found for ZIP code', zip_code))
+  return(tracts)
+}
+
