446e62a7e801eaffc734717b1fd4c9cfa1ddf092
Gavin Rozzi <gr@gavinrozzi.com>
2021-08-28 19:59:16 -0400

    Continue download refactor, update data

--- R/download_data.r
+++ R/download_data.r
@@ -17,25 +17,17 @@
 download_zip_data <- function(force = FALSE) {
 
   # Define URLs for downloading external datasets used in the package
-  url_crosswalk <- "https://github.com/gavinrozzi/zipcodeR-data/blob/master/zip_to_cd.rda?raw=true"
+  url_crosswalk <- "https://github.com/gavinrozzi/zipcodeR-data/blob/master/zcta_crosswalk.rda?raw=true"
   url_cd <- "https://github.com/gavinrozzi/zipcodeR-data/blob/master/zip_to_cd.rda?raw=true"
-
-  # Test if data dir exists and create
-  if (dir.exists(paste0(system.file(package = "zipcodeR"), "/data")) == TRUE) {
-    cat("Data directory exists - moving to download phase")
-  } else if (dir.exists(paste0(system.file(package = "zipcodeR"), "/data")) == FALSE) {
-    cat("Creating data directory")
-    dir.create(paste0(system.file(package = "zipcodeR"), "/data"))
-  }
 
   # Test if ZCTA crosswalk file exists, download if not present
   if (file.exists(system.file("data", "zcta_crosswalk.rda", package = "zipcodeR")) == TRUE && force == FALSE) {
     cat("Crosswalk file found, skipping")
   } else if (file.exists(system.file("data", "zcta_crosswalk.rda", package = "zipcodeR")) == FALSE) {
-    cat("Downloading ZCTA crosswalk file")
+    cat(paste("zipcodeR: Downloading ZCTA crosswalk file","\n"))
     utils::download.file(url_crosswalk, paste0(system.file("data", package = "zipcodeR"), "/zcta_crosswalk.rda"))
   } else if (force == TRUE) {
-    cat("Forcing Download of ZCTA crosswalk file")
+    cat(paste("zipcodeR: forcing Download of ZCTA crosswalk file","\n"))
     utils::download.file(url_crosswalk, paste0(system.file("data", package = "zipcodeR"), "/zcta_crosswalk.rda"))
   }
 
@@ -61,9 +53,6 @@ download_zip_data <- function(force = FALSE) {
   # Store the latest download URL from GitHub
   file_name <- zip_db_url$browser_download_url
 
-  # Store the latest version of zip_code_db to internal package data
-  version <- as.Date(zip_db_url$created_at)
-  save(version, file = paste0(system.file("R", package = "zipcodeR"), "/sysdata.rda"))
 
   # create a temporary directory and file for downloading the data
   td <- tempdir()
@@ -90,8 +79,12 @@ download_zip_data <- function(force = FALSE) {
   # Read in the new data
   zip_code_db <- dbGetQuery(conn, "SELECT * FROM simple_zipcode")
 
-  # Save the updated RData file to package data directory
+  # Save the updated zip_code_db file to package data directory
   save(zip_code_db, file = paste0(system.file("data", package = "zipcodeR"), "/zip_code_db.rda"))
+
+  # Save the latest version of zip_code_db to internal package data
+  zip_code_db_version <- as.Date(zip_db_url$created_at)
+  save(zip_code_db_version, file = paste0(system.file("R", package = "zipcodeR"), "/sysdata.rda"))
 
   # Tear down the database connection
   RSQLite::dbDisconnect(conn)
@@ -100,7 +93,7 @@ download_zip_data <- function(force = FALSE) {
   if (file.exists(system.file("data", "zip_to_cd.rda", package = "zipcodeR")) == TRUE && force == FALSE) {
     cat("Congressional district file found, skipping")
   } else if (file.exists(system.file("data", "zip_to_cd.rda", package = "zipcodeR")) == FALSE) {
-    cat("Downloading congressional district data file")
+    cat("zipcodeR: Downloading congressional district data file")
     utils::download.file(url_cd, paste0(system.file("data", package = "zipcodeR"), "/zip_to_cd.rda"))
   } else if (force == TRUE) {
     cat("Forcing download of congressional district data file")
